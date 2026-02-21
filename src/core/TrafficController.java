66ff4bfdce545450d0b464993aaafacca1a0de78
Harshita Sinha <harshitasinha5657@gmail.ccom>
2026-02-21 20:02:16 +0530

    update

--- src/core/TrafficController.java
+++ src/core/TrafficController.java
@@ -5,31 +5,35 @@ public class TrafficController {
     private TrafficSignal signal;
 
     public TrafficController() {
-        signal = new TrafficSignal("RED", 30);
-    }
-
-    public void changeSignal() {
-
-        if (signal.getColor().equals("RED")) {
-            signal.setColor("GREEN");
-            signal.setTimer(20);
-
-        } else if (signal.getColor().equals("GREEN")) {
-            signal.setColor("YELLOW");
-            signal.setTimer(5);
-
-        } else {
-            signal.setColor("RED");
-            signal.setTimer(30);
-        }
-
-        signal.display();
+        signal = new TrafficSignal("RED", 5);
     }
 
     public void startSystem() {
-        signal.display();
-        changeSignal();
-        changeSignal();
-        changeSignal();
+
+        while (true) {
+
+            signal.setColor("RED");
+            signal.setTimer(5);
+            signal.display();
+            sleep();
+
+            signal.setColor("GREEN");
+            signal.setTimer(5);
+            signal.display();
+            sleep();
+
+            signal.setColor("YELLOW");
+            signal.setTimer(3);
+            signal.display();
+            sleep();
+        }
+    }
+
+    private void sleep() {
+        try {
+            Thread.sleep(2000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
     }
 }
