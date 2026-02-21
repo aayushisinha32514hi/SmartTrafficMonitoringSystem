d71f95cca926f8c7e0b341a4a147f52fd7f19e42
Harshita Sinha <harshitasinha5657@gmail.ccom>
2026-02-21 19:23:03 +0530

    Added backend logic

--- src/core/TrafficSignal.java
+++ src/core/TrafficSignal.java
@@ -1,4 +1,32 @@
 package core;
 
 public class TrafficSignal {
+
+    private String color;
+    private int timer;
+
+    public TrafficSignal(String color, int timer) {
+        this.color = color;
+        this.timer = timer;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public int getTimer() {
+        return timer;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public void setTimer(int timer) {
+        this.timer = timer;
+    }
+
+    public void display() {
+        System.out.println("Signal: " + color + " | Time: " + timer + " seconds");
+    }
 }
